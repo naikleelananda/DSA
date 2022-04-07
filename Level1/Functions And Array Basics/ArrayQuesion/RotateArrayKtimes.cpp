04b51572a4b5a846b60ec4fea958256cbfaf2de5
Leelananda Naik <71961848+naikleelananda@users.noreply.github.com>
2022-04-07 19:15:44 +0530

    Create RotateArrayKtimes.cpp

---
+++ Level1/Functions And Array Basics/ArrayQuesion/RotateArrayKtimes.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+void reverse(int* arr, int start , int end ){
+       while (start <= end )
+       {
+          swap(arr[start ], arr[end]);
+          start++;
+          end--;
+       }
+}
+void rotate(int* arr, int n, int k){
+   
+    k = k % n;
+    if( k < 0) {
+        k = k + n;
+    }
+
+    reverse(arr , 0 , n-k-1);
+    reverse(arr , n-k , n-1);
+    reverse(arr, 0 , n-1);
+    
+}
+
+void display(int* arr, int n){
+    for(int i = 0 ; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n, r;
+    cin>>n;
+    int* arr = new int[n];
+    for(int i = 0 ; i < n; i++){
+        cin>>arr[i];
+    }
+    cin>>r;
+    
+    rotate(arr,n,r);
+    display(arr,n);
+}
